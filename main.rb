d7777ce9d3919e9a61f623c4307e7e974cd25544
aiya000 <aiya000.develop@gmail.com>
2020-07-31 14:55:59 +0900

    :+1: Say Japanese

--- main.rb
+++ main.rb
@@ -3,15 +3,14 @@ require 'aws-sdk'
 credentials = Aws::SharedCredentials.new(profile_name: 'default')
 polly = Aws::Polly::Client.new(region: 'ap-northeast-1', credentials: credentials)
 
-begin
-  polly.synthesize_speech({
-    voice_id: 'Joanna',
-    text: 'Hello',
-    text_type: 'text',
-    output_format: 'mp3',
-    response_target: 'output.mp3',
-  })
-rescue Aws::Polly::Errors::ServiceError => e
-  puts 'Fatal error!'
-  puts e
-end
+response = polly.synthesize_speech({
+  voice_id: 'Mizuki',
+  language_code: 'ja-JP',
+  text: 'にっこにっこにー',
+  text_type: 'text',
+  output_format: 'mp3',
+  response_target: 'output.mp3',
+})
+
+puts 'Result:'
+puts response
